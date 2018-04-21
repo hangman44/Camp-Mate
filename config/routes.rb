29e5ffdd59d640cdbe64dd4329f7da869be42412
CampMate <burakgs44@gmail.com>
2018-04-21 23:42:41 +0000

    Add edit update functionality and partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :users
   resources :events
+  resources :users, except: [:new]
   root 'pages#home'
+  get 'signup', to: 'users#new'
   get 'pages/home', to: 'pages#home'
   get 'about', to: 'pages#about'
 end
