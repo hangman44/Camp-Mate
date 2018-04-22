34b7c149ac6d59a3708b8afecf2f5abacbf5e570
CampMate <burakgs44@gmail.com>
2018-04-22 00:21:50 +0000

    Add users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,10 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
+  def index
+    @users = User.all
+  end
  
   def create
    @user = User.new(user_params)
@@ -23,7 +26,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was updated successfully"
-      redirect_to articles_path
+      redirect_to events_path
     else
       render 'edit'
     end
