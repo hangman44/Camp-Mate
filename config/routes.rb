5e270f969bfc183ac5c3902dfb31ae93c1bed548
CampMate <burakgs44@gmail.com>
2018-04-22 20:41:51 +0000

    Create login form

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :events
   resources :users, except: [:new]
   root 'pages#home'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
   get 'pages/home', to: 'pages#home'
   get 'about', to: 'pages#about'
