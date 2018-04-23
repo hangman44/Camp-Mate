97058b7bfb5f48d55c108f357fcc49d26b12667f
CampMate <burakgs44@gmail.com>
2018-04-23 00:08:24 +0000

    Add signin upon sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
    if @user.save
+     session[:user_id] = @user.id
      flash[:success] = "Welcome to the CampMate #{@user.username}"
-     redirect_to events_path
+     redirect_to user_path(@user)
    else
     render 'new'
    end
