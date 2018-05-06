5185d84ba91f438324f36463f2a901e6e5088737
CampMate <burakgs44@gmail.com>
2018-05-06 23:30:42 +0000

    Date format

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true, length: {minimum: 10, maximum: 300}
   validates :user_id, presence: true
+  validates :location, presence: true, length: {minimum: 10, maximum: 300}
+  validates :price, presence: true
+  validates :date, presence: true, numericality: { only_integer: true }, length: {minimum: 10, maximum: 10}, acceptance: { message: 'Invalid Format!' }
+  
 end
